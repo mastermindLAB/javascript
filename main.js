da832e9dcf67e613f0e4f6775478327274de9eaf
suran <suran.cse@gmail.com>
2018-09-05 19:52:17 -0400

    new code on js file

--- main.js
+++ main.js
@@ -1,1 +1,20 @@
+var name = "bottle";
+var count = 99;
 
+while (count > 0){
+    console.log (count+ " numbers of " + name + " in the store ");
+    console.log ( "take one done from " + count + " and pass it arround");
+    count = count - 1;
+    if (count == 0){
+        console.log("close the business stock "+count);
+        
+}
+    else {
+        console.log(name+ " left " +count);
+    }
+    if (count <= 5){
+        alert(count+" bottle left need to stock up soon");
+        console.log( count+" bottle left need to stock up soon")
+    }
+   
+}
